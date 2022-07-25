af54d09c77a0488588816fa082a353a3191895f1
takumisaito <jobtech.saitou@gmail.com>
2022-07-25 11:08:39 +0900

    7/25.7

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import os
 
-import sys
-
-sys.path.append('c:\users\user\appdata\local\packages\pythonsoftwarefoundation.python.3.10_qbz5n2kfra8p0\localcache\local-packages\python310\site-packages')
-
 import requests
 import slackweb
 from flask import Flask, request, abort
